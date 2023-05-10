d633bd0f13ecfcaa0187d4845c0511fae7fda573
ZBM Zubayer <zbmzubayer@gmail.com>
2023-05-10 14:02:15 +0600

    Database configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234567890',
+      database: 'DesiDukaanDB',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    DbModule,
+  ],
   controllers: [],
   providers: [],
 })
