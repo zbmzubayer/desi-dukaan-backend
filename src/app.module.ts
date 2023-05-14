41b34b79ee804c667ec11a4ec07ef1a2c8fd14fe
ZBM Zubayer <zbmzubayer@gmail.com>
2023-05-15 02:18:33 +0600

    First production build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { CustomerPaymentModule } from './customer-payment/customer-payment.module';
 import { CustomerModule } from './customer/customer.module';
@@ -11,19 +14,22 @@ import { PaymentModule } from './payment/payment.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { SellerModule } from './seller/seller.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: 'containers-us-west-117.railway.app',
+      port: 7256,
       username: 'postgres',
       password: '1234567890',
-      database: 'DesiDukaanDB',
+      database: 'railway',
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../public'),
+      serveRoot: '/public/',
     }),
     DbModule,
     CustomerModule,
